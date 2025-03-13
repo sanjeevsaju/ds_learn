d8b809377f25f84f5db7b70c311c682703ccf9ac
sanjeevsaju <sanjeevsaju8281@gmail.com>
2025-03-13 22:59:27 +0530

    factorial recursive

--- learn.cpp
+++ learn.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 
-void reverseDigits(int n, int &revNum, int &basePos) {
-	if(n > 0) {
-		reverseDigits(n/10, revNum, basePos);
-		revNum += (n % 10) * basePos;
-		basePos *= 10;
+int factorial(int n) {
+
+	int res = 1;
+	for(int i = 2; i <= n; i++) {
+		res *= i;
 	}
+
+	return res;
 }
 
-int main() {
-	int n = 1234567;
-	int revNum = 0;
-	int basePos = 1;
-	reverseDigits(n, revNum, basePos);
-	std::cout<<"Reversed number using recursion is "<<revNum<<std::endl;
+int factorial_recursive(int n) {
+	if(n == 0 || n == 1)
+		return 1;
+	return n * factorial(n - 1);
+}
+
+int main(){
+
+	int n = 15;
+	std::cout<<"Factorial of 15 is "<<factorial(n)<<std::endl;
+
 	return 0;
 }
+
