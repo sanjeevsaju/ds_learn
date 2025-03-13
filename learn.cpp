dc3e937dcc5094fa90448117e34711369227df4b
sanjeevsaju <sanjeevsaju8281@gmail.com>
2025-03-13 10:02:16 +0530

    Sum of GP

--- learn.cpp
+++ learn.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+int sum_of_gp(int a, int r, int n) {
+	return (a * (1 - std::pow(r, n)))/(1 - r);
+}
 
 int main() {
-	std::cout<<"Hello World!"<<std::endl;
+	//Sum of GP
+	int a = 2, r = 2, n = 15;
+	std::cout<<"Sum of GP is "<<sum_of_gp(a, r, n)<<std::endl;
 	return 0;
 }
