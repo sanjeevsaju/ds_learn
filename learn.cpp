b678d95c26f4eaf442682e5086299086929dfb8d
sanjeevsaju <sanjeevsaju8281@gmail.com>
2025-03-24 23:19:24 +0530

    Second Largest in an array

--- learn.cpp
+++ learn.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <vector>
 
-int largestElement(int arr[], int size) {
+int secondLargestElement(int arr[], int size) {
 	int largest = -1;
+	int secondLargest = -1;
 
 	for(int i = 0; i < size; i++) {
-		if(arr[i] > largest)
+		if(arr[i] > largest) {
+			secondLargest = largest;
 			largest = arr[i];
+		} else if(arr[i] < largest && arr[i] > secondLargest) {
+			secondLargest = arr[i];
+		}
 	}
 
-	return largest;
+	return secondLargest;
 }
 
 int main(){
-	int arr[] = {1, 2, 3, 4, 5, 6};
+	int arr[] = {11, 2, 10, 4, 5, 6};
 	int size = sizeof(arr) / sizeof(arr[0]);
-	std::cout<<largest(arr, size)<<std::endl;
+	std::cout<<secondLargestElement(arr, size)<<std::endl;
 	return 0;
 }
 
