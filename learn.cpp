fc2f4080909c22ff8fc6ee9ffc5c39e1e3f5a9cd
sanjeevsaju <sanjeevsaju8281@gmail.com>
2025-03-30 12:37:23 +0530

    Stock Market

--- learn.cpp
+++ learn.cpp
@@ -1,52 +1,45 @@
 #include <iostream>
 #include <vector>
-void distinctElementsSort(std::vector<int> arr, std::vector<int> &d_arr) {
-	
-	std::sort(arr.begin(), arr.end());
-	for(int i = 0; i < arr.size(); i++) {
-		if(i == 0 || arr[i] != arr[i - 1])
-			d_arr.push_back(arr[i]);
+
+int maximumProfitAccumulation(std::vector<int> prices) {
+	int n = prices.size();
+	int res = 0;
+
+	for(int i = 1; i < n; i++) {
+		if(prices[i] > prices[i - 1])
+			res += prices[i] - prices[i - 1];
 	}
+
+	return res;
 }
 
-void distinctElements(std::vector<int> arr, std::vector<int> &d_arr) {
-	
+int maximumProfitMaximaMinima(std::vector<int> arr) {
 	int n = arr.size();
-	for(int i = 0; i < n; i++){
-		int j;
-		for(j = 0; j < i; j++) {
-			if(arr[i] == arr[j])
-				break;
-		}
-		if(i == j)
-			d_arr.push_back(arr[i]);
+	int lMin = arr[0];
+	int lMax = arr[0];
+	int i = 0;
+	int res = 0;
+
+	while(i < n) {
+
+		while(i < n && arr[i] >= arr[i + 1]) { i++; }
+		lMin = arr[i];
+
+		while(i < n && arr[i] <= arr[i + 1]) { i++; }
+		lMax = arr[i];
+
+		res += (lMax - lMin);
 	}
 
+	return res;
 }
-
-bool duplicateCheck(std::vector<int> arr, int k) {
 	
-	int n = arr.size();
-	for(int i = 0; i < k; i++){
-		int j;
-		for(j = 0; j < i; j++) {
-			if(arr[i] == arr[j] && i != j)
-				return true;
-		}
-	}
-
-	return false;
-
-}
-
 
 int main() {
 
-	std::vector<int> arr = {1, 0, 4, 66, 7, 1, 0, 65, 0, 0, 3};
-	if(duplicateCheck(arr, 6))
-		std::cout<<"There are duplicates"<<std::endl;
-	else
-		std::cout<<"There are no duplicates"<<std::endl;
+	std::vector<int> prices = {100, 180, 260, 310, 40, 535, 695};
+	int maxProfit = maximumProfitAccumulation(prices);
+	std::cout<<maxProfit<<std::endl;
 
 	return 0;
 }
